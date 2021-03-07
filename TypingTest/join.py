b1361b71222441901f95a50389d5b0ab5d770020
AmatureHour <cleandfw@gmail.com>
2021-03-07 00:22:38 -0600

    connected the DB and the join page

--- TypingTest/join.py
+++ TypingTest/join.py
@@ -14,14 +14,46 @@ def join(stdscr):
     curses.resize_term(40, 80) 
     curses.update_lines_cols() 
     stdscr.keypad(True)
+
+    
     stdscr.addstr(10, 32, "CREATE A USERNAME")
+    stdscr.addstr(15, 25, "When completed you will be redirected")
+    stdscr.addstr(16, 32, "to the main page to log in")
     stdscr.addstr(20, 32, "CREATE A PASSWORD")
     
-    username = stdscr.getstr(12, 32, 15)
+    stdscr.move(12, 32)
+    username = ''
+    key = None
+    while key != 10:
+        key = stdscr.getch()
+        if key == 27:
+            curses.curs_set(0)
+            return 0
+        elif key == 8:
+            username = username[:-1]
+            stdscr.delch()
+        else:
+            username += str(key)
     
-    password = stdscr.getstr(22, 32, 15)
+    stdscr.addstr(8, 25, usersetup.user_look_up(username))
+
+
+    stdscr.move(22, 32)
+    password = ''
+    key = None
+    while key != 10:
+        key = stdscr.getch()
+        if key == 27:
+            curses.curs_set(0)
+            return 0
+        elif key == 8:
+            password = password[:-1]
+            stdscr.delch()
+        else:
+            password += str(key)
     
+    password = bytes(password, "utf-8")
     hashedpw = bcrypt.hashpw(password, bcrypt.gensalt())
     usersetup.init_user(str(username), str(hashedpw))
     curses.curs_set(0)
-    return username, password
+    
--- TypingTest/usersetup.py
+++ TypingTest/usersetup.py
@@ -3,10 +3,10 @@ import sqlite3 as sl
 import string
 
 
+
 def init_user(un, pswd):
     conn = sl.connect("userdata.sqlite")
     cur = conn.cursor()
-    
 
     cur.execute("""
         CREATE TABLE IF NOT EXISTS Users (
@@ -16,32 +16,39 @@ def init_user(un, pswd):
             )        
     """)
 
-    
-    cur.execute("""CREATE TABLE IF NOT EXISTS {} (
+    conn.commit()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Stats (
             Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-            Key TEXT UNIQUE,
-            Total INTEGER,
-            TotalWords REAL,
-            TotalTime REAL,
-            Completed INTEGER,
-            WpmAverage REAL,
-            3Missed TEXT 
+            TotalWords REAL DEFAULT 0.0,
+            TotalTime REAL DEFAULT 0.0,
+            Completed INTEGER DEFAULT 0,
+            WpmAverage REAL DEFAULT 0.0,
+            MissedKeys TEXT,
+            MissedMost TEXT,
+            UserId INTEGER
             )
-    """.format(un))
+    """)
    
-    printable = list(string.printable)
-    tots = [0] * len(printable) 
-    base = zip(printable, tots)
     cur.execute("""
         INSERT OR IGNORE INTO Users (UserName, Password) VALUES (?,?)
     """, (un, pswd))
     
-    cur.executemany("""INSERT OR IGNORE INTO {} (Key, Total) VALUES (?,?) 
-    """.format(un), base)
-    
-    cur.execute("""INSERT OR IGNORE INTO {} (
-        TotalWords, TotalTime, Completed, WpmAverage, 3Missed
-        ) VALUES (?,?,?,?,?)
-    """.format(un), (0,0,0,0,0))
     conn.commit()
     conn.close()
+
+
+def user_look_up(un):
+    conn = sl.connect("userdata.sqlite")
+    cur = conn.cursor()
+    out = ''
+    cur.execute("""
+        SELECT UserName FROM Users WHERE UserName =?
+    """, (un,))
+    if len(cur.fetchall()) > 0:
+        out += "there is already a user by that name"
+
+    else:
+        out += "Welcome "+str(un)
+    conn.close()
+    return out
